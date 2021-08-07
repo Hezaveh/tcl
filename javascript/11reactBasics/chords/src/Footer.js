eddca3d28338d6bf07df996c5e8ae379ef4926cf
Hezaveh <Mari.hezaveh@gmail.com>
2021-08-07 15:56:45 -0400

    Delete javascript directory

--- javascript/11reactBasics/chords/src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function Footer(){
-    return(
-        <foorer>This is the footer!</foorer>
-    )
-}
-
-export default Footer
--- javascript/6NextLevelJavaScript/ES6/example.js
+++
@@ -1,4 +0,0 @@
-//export const data = [1,2,3];
-
-const data = [1,2,3];
-module.exports = data
